f5d9fdcd2cc0930bc73ac9a9e2913bc3817fdc70
vrittech-manoj <manoj@vrittechnologies.com>
2024-12-26 15:00:43 +0545

    .

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from botasaurus.browser import browser, Driver
 
-from botasaurus.browser import browser, Driver
-
-@browser(driver_path="/usr/bin/chromedriver")
+@browser
 def scrape_heading_task(driver: Driver, data):
+    # Set the driver path manually
+    driver.set_driver_path("/usr/bin/chromedriver")
+    
+    # Visit the website
     driver.get("https://www.omkar.cloud/")
+    
+    # Retrieve the heading element's text
     heading = driver.get_text("h1")
+
+    # Save the data as a JSON file
     return {"heading": heading}
 
-# Initiate the web scraping task
+# Run the web scraping task
 scrape_heading_task()
