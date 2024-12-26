b24cc2db9589d8dc09107e3d690b4b8e73d509f8
vrittech-manoj <manoj@vrittechnologies.com>
2024-12-26 15:25:01 +0545

    .

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from botasaurus.browser import browser, Driver
 
-@browser
+# Define the function to add arguments
+def add_arguments(data, options):
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--no-sandbox')
+
+@browser(headless=True)
 def scrape_heading_task(driver: Driver, data):
-    # Set the driver path manually
-    driver.set_driver_path("/usr/bin/chromedriver")
-    
     # Visit the website
     driver.get("https://www.omkar.cloud/")
     
     # Retrieve the heading element's text
     heading = driver.get_text("h1")
 
-    # Save the data as a JSON file
+    # Return the heading
     return {"heading": heading}
 
 # Run the web scraping task
